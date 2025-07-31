d444be8b0820de25082663f87959edad97bb9855
TahaBo04 <tahab896@gmail.com>
2025-07-31 19:52:39 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    data = {
+        "name": "Soumaya",
+        "stamp": "August 1, 2025",
+        "mem_date": {
+            "first_date": "3rd of April • Arribat Center → Mega Mall",
+            "first_touch": "Marina Salé • first time we held hands",
+            "first_kiss": "12th of July • sunset"
+        },
+        "admires": ["Your serenity" , "beauty" , "love" , "compassion" , "I love everything about you down to your last minute detail"],
+        "promise": "I promise to keep loving you—if not the same way I do now, then more. All of my love will be for you, and you only.",
+        # Code-unlock messages (you’ll text her the codes at those times)
+        "codes": [
+            {"code": "ROSE",  "label": "Morning Gift", "msg": "You are my first thought every morning."},
+            {"code": "SMILE", "label": "Noon Gift",    "msg": "Even from afar, your smile is my sunshine."},
+            {"code": "STAR",  "label": "Evening Gift", "msg": "The sunset will never be as beautiful as our first kiss."},
+            {"code": "KISS",  "label": "Night Gift",   "msg": "Sleep peacefully, my forever. I am yours in every dream."}
+        ]
+    }
+    return render_template("index.html", **data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
